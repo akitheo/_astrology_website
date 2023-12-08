0ecde5d1485fba711ea6777de57a151bc05b5af1
Akash Pal <apal707662@gmail.com>
2023-12-08 23:50:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,27 +11,24 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json()); // Parse JSON bodies
-
 app.use(cors());
-
-
 // Serve HTML form
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'form.html'));
 });
 
 // Handle form submission
-app.post('/submit', async (req, res) => {
+app.post('/submit-form', async (req, res) => {
   try {
-    // Extract form data
-    const { firstName, lastName, dob, birthTime, phoneNumber, email } = req.body;
+    // Extract form data from FormData object
+    const formData = req.body;
 
     // Nodemailer configuration
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,  // Corrected line
-        pass: process.env.EMAIL_PASSWORD,  // Corrected line
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
 
@@ -41,11 +38,11 @@ app.post('/submit', async (req, res) => {
       to: 'apal707662@gmail.com', // Replace with the astrologer's email address
       subject: 'New Consultation Booking',
       html: `
-        <p>Name: ${firstName} ${lastName}</p>
-        <p>Date of Birth: ${dob}</p>
-        <p>Birth Time: ${birthTime}</p>
-        <p>Phone Number: ${phoneNumber}</p>
-        <p>Email: ${email}</p>
+        <p>Name: ${formData.get('firstName')} ${formData.get('lastName')}</p>
+        <p>Date of Birth: ${formData.get('dob')}</p>
+        <p>Birth Time: ${formData.get('birthTime')}</p>
+        <p>Phone Number: ${formData.get('phoneNumber')}</p>
+        <p>Email: ${formData.get('email')}</p>
       `,
     };
 
@@ -53,12 +50,17 @@ app.post('/submit', async (req, res) => {
     const info = await transporter.sendMail(mailOptions);
     console.log('Email sent:', info.response);
 
-    // Redirect to a success page
-    res.sendFile(path.join(__dirname, 'success.html'));
+    // Respond with a success message
+    res.status(200).send('Form submission successful');
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error('Error processing form submission:', error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 // Start the server
